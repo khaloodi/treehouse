714e2d1346462a8d5c62afcb4b0dd5be9accaa93
Khaled Adad <khaledadad@lewisu.edu>
2020-11-01 11:59:50 -0600

    Creating empty array

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -4,30 +4,36 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
-***/
-
+ ***/
+const quotes = [
+    {},
+    {},
+    {},
+    {},
+    {}
+]
 
 
 /***
  * `getRandomQuote` function
-***/
+ ***/
 
 
 
 /***
  * `printQuote` function
-***/
+ ***/
 
 
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
-
+ ***/
+console.log('test');
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
