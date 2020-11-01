2e0cbec02e651d52ab6669fdcc3023de314e5be5
Khaled Adad <khaledadad@lewisu.edu>
2020-11-01 12:35:39 -0600

    Added quotes

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,30 +10,45 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
  ***/
-const quotes = [
-    {},
-    {},
-    {},
-    {},
-    {}
-]
+const quotes = [{
+            source: 'Robin Sharma',
+            quote: 'Make your faith larger than your fears and your dreams larger than your doubts.'
+        },
+        {
+            source: 'Dr. Seuss',
+            quote: 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.'
+        },
+        {
+            source: 'Babe Ruth',
+            quote: 'Never let the fear of striking out keep you from playing the game.'
+        },
+        {
+            source: 'Nelson Mandela',
+            quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+            {
+                source: 'Casey Neistat',
+                quote: 'Do more.',
+                citation: 'YouTube',
+                year: 2017
+            }
+        ]
 
 
-/***
- * `getRandomQuote` function
- ***/
+        /***
+         * `getRandomQuote` function
+         ***/
 
 
 
-/***
- * `printQuote` function
- ***/
+        /***
+         * `printQuote` function
+         ***/
 
 
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
- ***/
-console.log('test');
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+        /***
+         * click event listener for the print quote button
+         * DO NOT CHANGE THE CODE BELOW!!
+         ***/
+        console.log('test');
+        document.getElementById('load-quote').addEventListener("click", printQuote, false);
