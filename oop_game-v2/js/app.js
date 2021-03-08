c1bfec2113bbf0feb1c6ff542458cf44a301891e
Khaled Adad <khaledadad@lewisu.edu>
2021-03-08 13:08:28 -0600

    game is restarting

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -2,59 +2,32 @@
  * Project 4 - OOP Game App
  * app.js */
 
-
-/* Test code to initialize my first phrase
-const phrase = new Phrase('Life is like a box of chocolates');
-console.log(`Phrase - phrase: ${phrase.phrase}`);
-*/
-
-/*
-game.phrases.forEach((phrase, index) => {
-  console.log(`Phrase ${index} - phrase: ${phrase.phrase}`)
-})
-*/
-
-/*
-const logPhrase = (phrase) => {
-  console.log(`Phrase - phrase: `, phrase.phrase);
-};
-
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-*/
-
-const game = new Game();
-
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game = '';
+let start = document.getElementById('btn__reset');
 const keyboard = document.getElementsByClassName('key');
+
+
 for (el of keyboard) {
     el.addEventListener('click', e => {
         game.handleInteraction(e.target);
     })
 }
 
-// const overlay = document.getElementById('overlay');
-if (document.getElementById('overlay').classList.contains('lose') || document.getElementById('overlay').classList.contains('win')) {
-    document.querySelector('#phrase ul').innerHTML = '';
-    for (el of keyboard) {
-        el.setAttribute('disabled', false)
-        el.classList.remove('wrong')
-        el.classList.remove('chosen')
+start.addEventListener('click', () => {
+    if (document.getElementById('overlay').classList.contains('lose') || document.getElementById('overlay').classList.contains('win')) {
+        document.querySelector('#phrase ul').innerHTML = '';
+        for (el of keyboard) {
+            el.setAttribute('disabled', false)
+            el.classList.remove('wrong')
+            el.classList.remove('chosen')
+        }
+        const scoreboard = document.querySelector('#scoreboard ol')
+        for (li of scoreboard) {
+            li.firstElementChild.src = "./images/liveHeart.png";
+        }
     }
-    const scoreboard = document.querySelector('#scoreboard ol')
-    for (li of scoreboard) {
-        li.firstElementChild.src = "./images/liveHeart.png";
-    }
-    // game.startGame();
+    game = new Game();
+    game.startGame();
     // console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
-    // const keyboard = document.getElementsByClassName('key');
-    // for (el of keyboard) {
-    //     el.addEventListener('click', e => {
-    //         game.handleInteraction(e.target);
-    //     })
-    // }
-}
+
+})
